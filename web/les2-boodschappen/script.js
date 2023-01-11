434978ec4c769a3a1b8edf6bc9418f347e282f6d
Eryk <35810@ma-web.nl>
2023-01-11 14:43:16 +0100

    les2

--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,21 @@
 console.log("script is geladen")
+
+const groentevak = document.getElementById("groente");
+const fruitvak = document.getElementById("fruit")
+
+function zetinfruitla(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement("h2");
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = "rood";
+    fruitvak.appendChild(nieuwElement)
+}
+
+function zetingroentenla(groenten){
+    console.log(groenten)
+
+    const nieuwElement = document.createElement("h2");
+    nieuwElement.innerHTML = groenten;
+    groentevak.appendChild(nieuwElement)
+}
